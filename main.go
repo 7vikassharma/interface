259e4a79d6d53802224ff7ec7dfb25ab8edb888f
Vikas Sharma <vikas97976464@gmail.com>
2023-07-09 06:27:17 +0000

    interface imp

--- main.go
+++ main.go
@@ -2,54 +2,46 @@ package main
 
 import "fmt"
 
-type WTest interface  {
-  Age(int)
-  Name(string)
-  Gender()
+type WTest interface {
+	Age(int)
+	Name(string)
+	Gender()
 }
-type MTest  interface {
-  WTest
-  Bhaii()
+type MTest interface {
+	WTest
+	Bhaii()
 }
-type Woman struct {}
+type Woman struct{}
 type Man struct {
-  Woman
-  single string
+	Woman
+	single string
 }
 
-func(w Woman) Age(age int){
-  fmt.Println(age)
+func (w Woman) Age(age int) {
+	fmt.Println(age)
 }
-func(w Woman) Name(name string){
-  fmt.Println(name)
+func (w Woman) Name(name string) {
+	fmt.Println(name)
 }
-func (m Woman) Gender(){
-  fmt.Println("f")
+func (m Woman) Gender() {
+	fmt.Println("f")
 }
-func(w Man) Age(age int){
-  fmt.Println(3*age)
+func (w Man) Age(age int) {
+	fmt.Println(3 * age)
 }
-func(m Man) Bhaii() {
-  m.single = "yes"
-  fmt.Println(m)
-} 
-
-
-
-func main(){ 
-  var in MTest
-  in = Man{}
-  in.Age(25)
-  in.Gender()
-  in.Bhaii()
-  var ini WTest
-  ini = Woman{}
-  ini.Age(35)
-  ini.Gender()  
+func (m Man) Bhaii() {
+	m.single = "yessss"
+	fmt.Println(m)
 }
 
-
-
-
-
-
+func main() {
+	var in MTest
+	in = Man{}
+	in.Age(25)
+	in.Gender()
+	in.Bhaii()
+	var ini WTest
+	ini = Woman{}
+	ini.Age(35)
+	ini.Gender()
+}
